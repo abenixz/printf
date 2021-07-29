e5bfbbdc3b3144b078f57933b66331b0485c6bcd
abenixz <abezelalem.0703@gmail.com>
2021-07-29 12:07:21 +0300

    Printf and holberton.h Added

--- holberton.h
+++ holberton.h
@@ -1,53 +1,32 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
+#include <stdarg.h>
 /**
- * struct print_buffer - structer for the write buffer.
- * @index: current index of the buffer.
- * @size: size of the buffer.
- * @overflow: this recoreds the overflow.
- * @str: pointer to memory that contains the content for this buffer.
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-typedef struct print_buffer
+typedef struct print
 {
-	size_t index;
-	size_t size;
-	size_t overflow;
-	char *str;
-} buffer;
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-buffer *buf_new();
-buffer *buf_custom(size_t);
-size_t buf_size(buffer *);
-size_t buf_index(buffer *);
-char *buf_content(buffer *);
-void buf_write(buffer *);
-void buf_end(buffer *);
-void buf_wr(buffer *);
-void buf_inc(buffer *);
-
-/**
- * struct print_ops - struct for the write operators.
- * @op: hold a symbol that represents the operator.
- * @fn: pointer function to the write functions.
- */
-typedef struct print_ops
-{
-	char *op;
-	int (*fn)(buffer *, va_list);
-} prtOp;
-
-prtOp *prtOp_init();
-
-void append_num(buffer *buf, unsigned int num);
-
-int write_bin(buffer *buf, va_list v_ls);
-
+int _putchar(char c);
 int _printf(const char *format, ...);
-int opid(buffer *buf, va_list v_ls, const char *src, int src_i);
-int write_char(buffer *buf, va_list v_ls);
-int write_str(buffer *buf, va_list v_ls);
-int write_mod(buffer *buf, va_list v_ls);
-int write_int(buffer *buf, va_list v_ls);
-char *itoc(int num, char *dest);
-#endif
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
+#endif  /* _HOLBERTON_H */
